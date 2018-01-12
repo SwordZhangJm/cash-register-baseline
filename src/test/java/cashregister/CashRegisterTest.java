3e86e476889e035987e545907cef2c09c283e01e
zhangjianming7 <zhangjianming@jd.com>
2018-01-12 22:17:04 +0800

    使用mockito

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -3,14 +3,17 @@ package cashregister;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
 public class CashRegisterTest {
 
-
     @Test
-    public void should_print_null_purchase() {
+    public void should_print_mock_purchase() {
         //initialize CashRegister and fake Printer
-
-        PrinterMock printer = new PrinterMock();
+        Printer printer = mock(Printer.class);
         CashRegister cashRegister = new CashRegister(printer);
 
         Item[] items = new Item[1];
@@ -18,11 +21,9 @@ public class CashRegisterTest {
         Purchase purchase = new Purchase(items);
 
         cashRegister.process(purchase);
-
-        Assert.assertEquals("dog\t100.0\n", printer.getData());
+        verify(printer,times(1)).print(purchase.asString());
         //verify that printer was called
     }
-
     @Test
     public void should_print_stub_purchase() {
         //initialize CashRegister and fake Printer
